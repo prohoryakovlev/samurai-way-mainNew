b6cbee87b68dd489099c62ea33215e812f5417bc
Prohor <prohor-yakovlev@mail.ru>
2022-12-07 19:02:12 +0300

    Add

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,33 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Technologies/>
+        </div>
+    );
+}
+
+const Technologies = () => {
+    return (
+        <ul>
+            <li>html</li>
+            <li>css</li>
+            <li>js</li>
+            <li>React</li>
+        </ul>
+    )
+}
+
+const Header = () => {
+    return (
+        <div>
+            <a href="">Home</a>
+            <a href="">News Feed</a>
+            <a href="">Messages</a>
+        </div>
+    )
 }
 
 export default App;
