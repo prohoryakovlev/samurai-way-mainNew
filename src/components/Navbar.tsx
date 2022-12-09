ab2b0a1f3d490640011a1c973ee316b4088f3989
Prohor <prohor-yakovlev@mail.ru>
2022-12-09 17:10:45 +0300

    Components css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import './Navbar.css'
-
+import classes from './Navbar.module.css';
 const Navbar = () => {
     return (
-            <nav className="nav">
-                <div className="item">
+            <nav className={classes.nav}>
+                <div className={classes.item}>
                     <a>Profile</a>
                 </div>
-                <div className="item">
+                <div className={classes.item}>
                     <a>Messages</a>
                 </div>
-                <div className="item">
+                <div className={classes.item}>
                     <a>News</a>
                 </div>
-                <div className="item">
+                <div className={classes.item}>
                     <a>Music</a>
                 </div>
-                <div className="item">
+                <div className={classes.item}>
                     <a>Settings</a>
                 </div>
             </nav>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import './Profile.css'
-
+import classes from './Profile.module.css';
 const Profile = () => {
     return (
             <div className="content">
@@ -16,8 +15,8 @@ const Profile = () => {
                     <div>
                         New post
                     </div>
-                    <div className='posts'>
-                        <div className='item'>
+                    <div className={classes.posts}>
+                        <div className={classes.item}>
                             Post1
                         </div>
                         <div className='item'>
