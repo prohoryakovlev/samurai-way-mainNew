43b5124a7c66fe22aa56aaefe171908efcfc7e5b
Prohor <prohor-yakovlev@mail.ru>
2022-12-11 13:53:58 +0300

    props

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,25 +2,22 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import Posts from "./Post/Posts";
 
-const MyPosts = () => {
+const MyPosts = (props: any) => {
     return (
-        <div>
             <div>
                 My posts
-
                 <textarea></textarea>
                 <button>Add post</button>
 
                 <div className={s.posts}>
 
-                    <Posts/>
-                    <Posts/>
-                    <Posts/>
-                    <Posts/>
+                    <Posts likeCount={15} message={"Hi, how are you?"}/>
+                    <Posts likeCount={20} message={"It s my first post"}/>
+
+
 
                 </div>
             </div>
-        </div>
     )
 };
 
--- src/components/Profile/MyPosts/Post/Posts.tsx
+++ src/components/Profile/MyPosts/Post/Posts.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import s from './Posts.module.css';
 
-const Posts = () => {
+const Posts = (props: any) => {
     return (
         <div className={s.item}>
-            <img
-                src="https://sun9-75.userapi.com/impg/bBKdP5AFq1kDbb0jdGHWyd1tteNOWKQjOQui3A/6lXvqAo5vyw.jpg?size=95x130&quality=96&sign=58bcba2039688f2ea2dd3c1fa5e214c6&c_uniq_tag=3RyIKuRp_Fn82-C9OnBFour5xfxr26zV0m5tZiJmgb4&type=album"/>
-            Post1
+            <img src="https://sun9-75.userapi.com/impg/bBKdP5AFq1kDbb0jdGHWyd1tteNOWKQjOQui3A/6lXvqAo5vyw.jpg?size=95x130&quality=96&sign=58bcba2039688f2ea2dd3c1fa5e214c6&c_uniq_tag=3RyIKuRp_Fn82-C9OnBFour5xfxr26zV0m5tZiJmgb4&type=album"/>
+            {props.message}
             <div>
-                <span>like</span>
+                <span>like {props.likeCount}</span>
             </div>
+
+
         </div>
     );
 };
