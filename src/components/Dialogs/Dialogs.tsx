2d7aefd68d41369f02c360687a1bd11b2feae22d
Prohor <prohor-yakovlev@mail.ru>
2022-12-18 14:25:51 +0300

    Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,9 +3,35 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div >
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={s.dialog}>
+                        Dimych
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Victor
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Hi</div>
+                <div className={s.dialog}>How is your it-kamasutra</div>
+                <div className={s.dialog}>Yo</div>
+
+            </div>
         </div>
+
     );
 };
 
